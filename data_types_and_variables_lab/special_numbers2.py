42cd92823c901ca5bcb79fd84da84eab65abf689
Tsveti1103 <cvstoqnova@gmail.com>
2022-01-12 16:00:30 +0200

    Create special_numbers2.py

---
+++ data_types_and_variables_lab/special_numbers2.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for num in range(1, n + 1):
+    temp_num = num
+    sum = 0
+    while temp_num > 0:
+        digit = temp_num % 10
+        sum += digit
+        temp_num = temp_num // 10
+        isSpecialNumber = sum == 5 or sum == 7 or sum == 11
+    print(f"{num} -> {isSpecialNumber}")
