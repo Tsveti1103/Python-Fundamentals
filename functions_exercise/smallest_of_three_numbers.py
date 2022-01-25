e1490f964ab0620077c6bcd630bd4dfee7c6bfe1
Tsveti1103 <cvstoqnova@gmail.com>
2022-01-25 16:05:55 +0200

    Create smallest_of_three_numbers.py

---
+++ functions_exercise/smallest_of_three_numbers.py
@@ -0,0 +1,8 @@
+def smallest_number(*args):
+    return min(args)
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+print(smallest_number(first_number, second_number, third_number))
