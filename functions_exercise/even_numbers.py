fa13c09964e9f342f4014e7eb15d793ca5e6104c
Tsveti1103 <cvstoqnova@gmail.com>
2022-01-25 16:05:42 +0200

    Create even_numbers.py

---
+++ functions_exercise/even_numbers.py
@@ -0,0 +1,7 @@
+numbers = input().split()
+numbers_as_digits = []
+for number in numbers:
+    numbers_as_digits.append(int(number))
+is_even = lambda x: x % 2 == 0
+result = list(filter(is_even, numbers_as_digits))
+print(result)
