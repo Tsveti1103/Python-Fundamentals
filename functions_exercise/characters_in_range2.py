ab76adb1ba153c649ddf3da78cb7a953569bb536
Tsveti1103 <cvstoqnova@gmail.com>
2022-01-27 16:06:32 +0200

    Create characters_in_range2.py

---
+++ functions_exercise/characters_in_range2.py
@@ -0,0 +1,8 @@
+def get_symbols(first, second):
+    return [chr(s) for s in range(ord(first) + 1, ord(second))]
+
+
+first_string = input()
+second_string = input()
+result = get_symbols(first_string, second_string)
+print(*result)
