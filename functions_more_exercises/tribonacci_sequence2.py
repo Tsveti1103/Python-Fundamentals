d8bec3a85a0bcf121004c5d0137cbb8edf44d37c
Tsveti1103 <cvstoqnova@gmail.com>
2022-01-30 16:10:16 +0200

    Create tribonacci_sequence2.py

---
+++ functions_more_exercises/tribonacci_sequence2.py
@@ -0,0 +1,11 @@
+length_of_list = int(input())
+new_list = [1]
+for i in range(length_of_list-1):
+    if len(new_list) < 3:
+        current_number = sum(new_list)
+        new_list.append(current_number)
+    else:
+        current_number = new_list[i] + new_list[i - 1] + new_list[i - 2]
+        new_list.append(current_number)
+
+print(*new_list)
