e5d3b818a641c266086063e5016a53fa9067d169
Tsveti1103 <cvstoqnova@gmail.com>
2022-01-30 16:10:12 +0200

    Create center_point.py

---
+++ functions_more_exercises/center_point.py
@@ -0,0 +1,20 @@
+import math
+
+
+def point_distance(x_point, y_point):
+    return x_point ** 2 + y_point ** 2
+
+
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+first_point_distance = point_distance(x1, y1)
+second_point_distance = point_distance(x2, y2)
+if first_point_distance <= second_point_distance:
+    x = math.floor(x1)
+    y = math.floor(y1)
+else:
+    x = math.floor(x2)
+    y = math.floor(y2)
+print(f"({x}, {y})")
