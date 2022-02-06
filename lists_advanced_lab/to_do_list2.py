0f630666c0ac07b9ad11d648b521bb0939bc2c0f
Tsveti1103 <cvstoqnova@gmail.com>
2022-02-06 16:12:15 +0200

    Create to_do_list2.py

---
+++ lists_advanced_lab/to_do_list2.py
@@ -0,0 +1,11 @@
+notes = [''] * 11
+command_input = input()
+while command_input != "End":
+    command_args = command_input.split("-")
+    priority = int(command_args[0])
+    note = command_args[1]
+    notes[priority] = note
+    command_input = input()
+
+filtered_notes = [n for n in notes if n != '']
+print(filtered_notes)
