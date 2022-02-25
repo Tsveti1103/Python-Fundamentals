a9b464f8e5ac96c213a112f0a2b5d41983b42da3
Tsveti1103 <cvstoqnova@gmail.com>
2022-02-25 16:22:16 +0200

    Create valid_usernames.py

---
+++ text_processing_exercise/valid_usernames.py
@@ -0,0 +1,29 @@
+def lenght(username):
+    if 3 <= len(username) <= 16:
+        return True
+    return False
+
+
+def type_of_symbols(username):
+    for character in username:
+        if not (character.isalnum() or character == "-" or character == "_"):
+            return False
+    return True
+
+
+def redundant(username):
+    for character in username:
+        if character == " ":
+            return False
+    return True
+
+
+def username_is_valid(username):
+    if lenght(username) and type_of_symbols(username) and redundant(username):
+        return True
+
+
+usernames = input().split(", ")
+for username in usernames:
+    if username_is_valid(username):
+        print(username)
