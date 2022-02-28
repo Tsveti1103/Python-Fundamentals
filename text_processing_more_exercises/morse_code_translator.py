3be8cfdb6dfc71b7c76154b68d992d72b1a6ab5c
Tsveti1103 <cvstoqnova@gmail.com>
2022-02-28 16:23:34 +0200

    Create morse_code_translator.py

---
+++ text_processing_more_exercises/morse_code_translator.py
@@ -0,0 +1,12 @@
+morse_code_dict = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
+                   '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P',
+                   '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+                   '-.--': 'Y', '--..': 'Z'}
+message = input().split(' | ')
+new = ""
+for text in message:
+    word = text.split()
+    for letter in word:
+        new += morse_code_dict[letter]
+    new += " "
+print(new)
